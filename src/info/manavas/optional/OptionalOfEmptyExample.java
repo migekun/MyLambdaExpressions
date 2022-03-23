a60806ee401f42c89cec5316ac51364d4e733a18
Miguel Ángel Navas <manavas@gmail.com>
2022-03-23 09:38:48 +0100

    Java8 Optional Examples IfIs OfEmpty orElse

---
+++ src/info/manavas/optional/OptionalOfEmptyExample.java
@@ -0,0 +1,23 @@
+package info.manavas.optional;
+
+import java.util.Optional;
+
+public class OptionalOfEmptyExample {
+    public static void main(String[] args) {
+        Optional<String> optionalString = Optional.of("Hello World");
+//        Optional<String> optionalString = Optional.of(null); //throw exception
+        System.out.println("optionalString = " + optionalString);
+        System.out.println("getWords() = " + getWords());
+    }
+    
+    public static Optional<String> getWords() {
+        String[] words = new String[10];
+        words[1] = "test";
+        Optional<String> optionalS = Optional.ofNullable(words[1]);
+        if (optionalS.isPresent()) {
+            return optionalS;
+        } else {
+            return Optional.empty();
+        }
+    }
+}
